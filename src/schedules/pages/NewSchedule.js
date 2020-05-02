f351ff5c9885bc40d90c1fda68fb5084a783fe20
stevepetebruce <stevepetebruce@hotmail.com>
2020-05-02 22:58:14 +0100

    Add Formik Use Formik to build forms and vaildation on the new schedule view.

--- src/schedules/pages/NewSchedule.js
+++ src/schedules/pages/NewSchedule.js
@@ -1,69 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import * as Yup from "yup";
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import {
-	VALIDATOR_REQUIRE,
-	VALIDATOR_MINLENGTH,
-} from "../../shared/util/validators";
-import useForm from "../../shared/hooks/form-hook";
 import "./ScheduleForm.css";
+import "../../shared/components/FormElements/Input.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+const validateSchema = Yup.object().shape({
+	title: Yup.string().required("Please enter the name of your event"),
+	description: Yup.string()
+		.min(10)
+		.required("Please enter a brief description of your event"),
+	address: Yup.string(),
+	startDate: Yup.date().required("A start date is required"),
+	endDate: Yup.date().required(
+		"An end date is required, even if it's the same date"
+	),
+});
 
 const NewSchedule = () => {
-	const [formState, inputHandler] = useForm(
-		{
-			title: {
-				value: "",
-				isValid: false,
-			},
-			description: {
-				value: "",
-				isValid: false,
-			},
-			address: {
-				value: "",
-				isValid: false,
-			},
-		},
-		false
-	);
-
-	const scheduleSubmitHandler = (e) => {
-		e.preventDefault();
-		console.log(formState.inputs); // Send to DB
-	};
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date(startDate));
 
 	return (
-		<form className='place-form' onSubmit={scheduleSubmitHandler}>
-			<Input
-				id='title'
-				element='input'
-				type='text'
-				label='Title'
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText='Please enter a valid title'
-				onInput={inputHandler}
-			/>
-			<Input
-				id='description'
-				element='textarea'
-				label='Description'
-				validators={[VALIDATOR_MINLENGTH(5)]}
-				errorText='Please enter a valid description (min 5 characters)'
-				onInput={inputHandler}
-			/>
-			<Input
-				id='address'
-				element='input'
-				label='Address'
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText='Please enter a valid address'
-				onInput={inputHandler}
-			/>
-			<Button type='submit' disabled={!formState.isValid}>
-				Add Schedule
-			</Button>
-		</form>
+		<Formik
+			initialValues={{
+				title: "",
+				description: "",
+				address: "",
+				startDate: "",
+				endDate: "",
+			}}
+			validationSchema={validateSchema}
+			validateOnChange={false}
+			validateOnBlur={false}
+			onSubmit={(values, { setSubmitting }) => {
+				setSubmitting(true);
+				// commit async call
+				console.log(values);
+				setSubmitting(false);
+			}}>
+			{({ values, isSubmitting, setFieldValue, touched, errors }) => (
+				<Form className='place-form'>
+					<label htmlFor='title'>Title</label>
+					<Field type='text' id='title' name='title' />
+					{errors.title && touched.title ? <div>{errors.title}</div> : null}
+					<label htmlFor='description'>Description</label>
+					<Field type='text' id='description' name='description' />
+					{errors.description && touched.description ? (
+						<div>{errors.description}</div>
+					) : null}
+
+					<label htmlFor='address'>Address</label>
+					<Field type='text' id='address' name='address' />
+					{errors.address && touched.address ? (
+						<div>{errors.address}</div>
+					) : null}
+					<label htmlFor='startDate'>Start Date</label>
+					<DatePicker
+						selected={startDate}
+						onChange={(startDate) => {
+							setFieldValue(
+								"startDate",
+								moment(startDate).format("DD MM YYYY")
+							);
+							setStartDate(startDate);
+							setEndDate(startDate);
+						}}
+						selectsStart
+						startDate={startDate}
+						endDate={endDate}
+						dateFormat='dd/MM/yyyy'
+						name='startDate'
+					/>
+					{errors.startDate && touched.startDate ? (
+						<div>{errors.startDate}</div>
+					) : null}
+					<label htmlFor='endDate'>End Date</label>
+					<DatePicker
+						selected={endDate}
+						onChange={(endDate) => {
+							setFieldValue("endDate", moment(endDate).format("DD MM YYYY"));
+							setEndDate(endDate);
+						}}
+						selectsEnd
+						startDate={startDate}
+						endDate={endDate}
+						minDate={startDate}
+						dateFormat='dd/MM/yyyy'
+						name='endDate'
+					/>
+					{errors.endDate && touched.endDate ? (
+						<div>{errors.endDate}</div>
+					) : null}
+
+					<button disabled={isSubmitting} type='submit'>
+						Add Schedule
+					</button>
+					<pre>{JSON.stringify(values)}</pre>
+				</Form>
+			)}
+		</Formik>
 	);
 };
 
