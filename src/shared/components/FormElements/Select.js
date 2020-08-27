5c0d546c449142416f3ccc9e7c01910b4e49a29f
stevepetebruce <stevepetebruce@hotmail.com>
2020-08-27 21:26:24 +0100

    Select pointer arrow alignment.

--- src/shared/components/FormElements/Select.js
+++ src/shared/components/FormElements/Select.js
@@ -10,27 +10,29 @@ function Select(props) {
 			<label htmlFor={name} className='text-gray-700'>
 				{label}
 			</label>
-			<Field
-				as='select'
-				id={name}
-				name={name}
-				{...rest}
-				className='form-input mt-1 block w-full'>
-				{[...Array(options)].map((num, i) => (
-					<option key={i + 1} value={i + 1}>
-						{i + 1}
-					</option>
-				))}
-			</Field>
-			<div className='pointer-events-none absolute inset-y-0 right-0 flex px-6 pt-10 h-1 text-gray-700'>
-				<svg
-					className='fill-current h-4 w-4'
-					xmlns='http://www.w3.org/2000/svg'
-					viewBox='0 0 20 20'>
-					<path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-				</svg>
+			<div className='relative'>
+				<Field
+					as='select'
+					id={name}
+					name={name}
+					{...rest}
+					className='form-input mt-1 block w-full'>
+					{[...Array(options)].map((num, i) => (
+						<option key={i + 1} value={i + 1}>
+							{i + 1}
+						</option>
+					))}
+				</Field>
+				<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+					<svg
+						className='fill-current h-4 w-4'
+						xmlns='http://www.w3.org/2000/svg'
+						viewBox='0 0 20 20'>
+						<path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+					</svg>
+				</div>
+				<ErrorMessage name={name} component={TextError} />
 			</div>
-			<ErrorMessage name={name} component={TextError} />
 		</>
 	);
 }
