d3c48408707a068e51dc476f822d496b447bcc08
stevepetebruce <stevepetebruce@hotmail.com>
2020-10-12 19:58:43 +0100

    Hide event images if user has not added one.

--- src/public_schedule/components/ScheduleDisplayEventItem.js
+++ src/public_schedule/components/ScheduleDisplayEventItem.js
@@ -53,13 +53,15 @@ function ScheduleDisplayEventItem({ event, timeDuration, eventDay }) {
 		<>
 			{parseInt(event.day) === parseInt(eventDay) && (
 				<div
-					className='bg-orange-100 hover:bg-orange-200 focus:outline-none focus:shadow-outline w-64 flex flex-row items-center rounded shadow absolute'
+					className='bg-orange-100 hover:bg-orange-200 focus:outline-none focus:shadow-outline w-64 h-16 flex flex-row items-center rounded shadow absolute'
 					style={style}>
-					<img
-						src='https://images.unsplash.com/photo-1560590053-465d3d493b62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
-						alt='Band'
-						className='h-16 w-16 object-cover rounded-l'
-					/>
+					{event.image && (
+						<img
+							src='https://images.unsplash.com/photo-1560590053-465d3d493b62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+							alt='Band'
+							className='h-16 w-16 object-cover rounded-l'
+						/>
+					)}
 					<div className='pl-3 overflow-x-auto'>
 						<h3 className='font-semibold text-lg leading-tight truncate text-blue-900'>
 							{event.presenter}
