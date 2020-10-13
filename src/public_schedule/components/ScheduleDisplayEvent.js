76c30525d5af245cd426529e803926e8286aff8d
stevepetebruce <stevepetebruce@hotmail.com>
2020-10-13 22:18:32 +0100

    Make the width of each row the full width of the event duration.

--- src/public_schedule/components/ScheduleDisplayEvent.js
+++ src/public_schedule/components/ScheduleDisplayEvent.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScheduleDisplayEventItem from "./ScheduleDisplayEventItem";
 
 function ScheduleDisplayEvent({ stages, eventList, timeDuration, eventDay }) {
+	const [style, setStyle] = useState({});
+
+	useEffect(() => {
+		if (timeDuration.length < 5) {
+			setStyle({
+				width: 300 * timeDuration.length + "px",
+			});
+		} else {
+			setStyle({
+				width: 200 * timeDuration.length + "px",
+			});
+		}
+	}, [timeDuration]);
+
 	return (
 		(eventList[stages[0]] && (
 			<>
 				{stages.map((stage) => (
 					<div
 						className='h-16 flex border-b border-blue-700 border-dashed w-screen relative'
-						key={stage}>
+						key={stage}
+						style={style}>
 						{eventList[stage].map((event) => (
 							<ScheduleDisplayEventItem
 								key={event.presenter}
