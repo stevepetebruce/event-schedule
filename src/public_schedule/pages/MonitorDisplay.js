bede109207f706d5c7ef5f8a7fdaabe1acb6a386
stevepetebruce <stevepetebruce@hotmail.com>
2020-10-24 18:01:10 +0100

    Fetch schedule data.

--- src/public_schedule/pages/MonitorDisplay.js
+++ src/public_schedule/pages/MonitorDisplay.js
@@ -1,8 +1,46 @@
-import React from "react";
+/* eslint-disable */
+import React, {useEffect, useState} from "react";
+
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 function MonitorDisplay() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+	const [data, setData] = useState();
+	
+	useEffect(() => {
+		const sendRequest = async() => {
+			setIsLoading(true);
+			try {
+				const response = await fetch("http://localhost:5000/api/schedules/display/5f209fffe85b9293b35d2fd6/3");
+				const responseData = await response.json();
+
+				if(!response.ok) {
+					throw new Error(responseData.message);
+				}
+				setData(responseData);
+			} catch (error) {
+				setError(error.message);
+			}
+			setIsLoading(false);
+
+		};
+		sendRequest();
+	},[]);
+
+	const errorHandler= () =>{ 
+		setError(null)
+	}
+
+	console.log(data);
+
+
 	return (
-		<div class='h-screen overflow-hidden w-full'>
+		<>
+		<ErrorModal error={error} onClear={errorHandler}/>
+		{isLoading && <LoadingSpinner asOverlay={true} />}
+		{!isLoading && data && (<div className='h-screen overflow-hidden w-full'>
 			<div className='px-20 pb-14 mx-auto flex justify-between content-center items-center flex-col sm:flex-row h-full'>
 				<div className='flex flex-col flex-grow justify-center sm:items-start pb-12'>
 					<div className='py-2 px-4 bg-red-600 text-gray-100 font-bold mb-2'>
@@ -12,7 +50,7 @@ function MonitorDisplay() {
 						13:00 - 13:30
 					</h4>
 					<h1 className='text-4xl md:text-5xl text-indigo-200 pr-2 font-bold leading-tight text-center sm:text-left'>
-						Band Name
+						Title
 					</h1>
 					<p className='my-10 leading-normal md:text-2xl mb-8 text-center sm:text-left'>
 						Main Stage
@@ -20,7 +58,8 @@ function MonitorDisplay() {
 				</div>
 				<div className='flex-grow-0 w-2/5'>Image location</div>
 			</div>
-		</div>
+		</div>)}
+		</>
 	);
 }
 
