1a4473799fad2cbf48877234baeaa588e8e2cc7d
stevepetebruce <stevepetebruce@hotmail.com>
2020-10-25 19:59:05 +0000

    Display data in view.

--- src/public_schedule/pages/MonitorDisplay.js
+++ src/public_schedule/pages/MonitorDisplay.js
@@ -8,6 +8,8 @@ function MonitorDisplay() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState();
 	const [data, setData] = useState();
+
+	const date = new Date();
 	
 	useEffect(() => {
 		const sendRequest = async() => {
@@ -33,32 +35,29 @@ function MonitorDisplay() {
 		setError(null)
 	}
 
-	console.log(data);
-
-
 	return (
 		<>
-		<ErrorModal error={error} onClear={errorHandler}/>
-		{isLoading && <LoadingSpinner asOverlay={true} />}
-		{!isLoading && data && (<div className='h-screen overflow-hidden w-full'>
-			<div className='px-20 pb-14 mx-auto flex justify-between content-center items-center flex-col sm:flex-row h-full'>
-				<div className='flex flex-col flex-grow justify-center sm:items-start pb-12'>
-					<div className='py-2 px-4 bg-red-600 text-gray-100 font-bold mb-2'>
-						UP NEXT
+			<ErrorModal error={error} onClear={errorHandler}/>
+			{isLoading && <LoadingSpinner asOverlay={true} />}
+			{!isLoading && data && (<div className='h-screen overflow-hidden w-full'>
+				<div className='px-20 pb-14 mx-auto flex justify-between content-center items-center flex-col sm:flex-row h-full'>
+					<div className='flex flex-col flex-grow justify-center sm:items-start pb-12'>
+						<div className='py-2 px-4 bg-red-600 text-gray-100 font-bold mb-2'>
+							UP NEXT
+						</div>
+						<h4 className='my-2 text-4xl md:text-5xl text-indigo-600 font-bold leading-tight text-center sm:text-left'>
+						{data.schedule.scheduleList[0].startTime} - {data.schedule.scheduleList[0].endTime}
+						</h4>
+						<h1 className='text-4xl md:text-5xl text-indigo-200 pr-2 font-bold leading-tight text-center sm:text-left'>
+							{data.schedule.scheduleList[0].etitle}
+						</h1>
+						<p className='my-10 leading-normal md:text-2xl mb-8 text-center sm:text-left'>
+							{data.schedule.scheduleList[0].stage}
+						</p>
 					</div>
-					<h4 className='my-2 text-4xl md:text-5xl text-indigo-600 font-bold leading-tight text-center sm:text-left'>
-						13:00 - 13:30
-					</h4>
-					<h1 className='text-4xl md:text-5xl text-indigo-200 pr-2 font-bold leading-tight text-center sm:text-left'>
-						Title
-					</h1>
-					<p className='my-10 leading-normal md:text-2xl mb-8 text-center sm:text-left'>
-						Main Stage
-					</p>
+					{data.schedule.scheduleList[0].image && <div className='flex-grow-0 w-2/5'>{data.schedule.scheduleList[0].image}</div>}
 				</div>
-				<div className='flex-grow-0 w-2/5'>Image location</div>
-			</div>
-		</div>)}
+			</div>)}
 		</>
 	);
 }
