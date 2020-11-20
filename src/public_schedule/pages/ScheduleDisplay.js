b127fb14c6a1662cd63a89b777d3e421057c9a04
stevepetebruce <stevepetebruce@hotmail.com>
2020-11-20 11:30:11 +0000

    Refactor schelule display component.

--- src/public_schedule/pages/ScheduleDisplay.js
+++ src/public_schedule/pages/ScheduleDisplay.js
@@ -25,86 +25,14 @@ const ScheduleDisplay = (props) => {
 	const [selectedTab, setSelectedTab] = useState(0);
 
 	const dateDifference = (date1, date2) => {
-		var oneDay = 1000 * 3600 * 24;
+		const oneDay = 1000 * 3600 * 24;
 		const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
 		const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-		var days = (utc2 - utc1) / oneDay;
+		const days = (utc2 - utc1) / oneDay;
 		return Math.abs(days);
 	}
 
-	useEffect(() => {
-		const scheduleDuration = (responseData) => {
-			const startTimes = responseData.schedule.scheduleList.map((schedule) => {
-				if (parseInt(schedule.startTime.split(":")[0]) < 5) {
-					return 24;
-				}
-				return parseInt(schedule.startTime.split(":")[0]);
-			});
-			const endTimes = responseData.schedule.scheduleList.map((schedule) => {
-				if (parseInt(schedule.endTime.split(":")[0], 10) < 5) {
-					return parseInt(schedule.endTime.split(":")[0]) + 24;
-				}
-				return parseInt(schedule.endTime.split(":")[0]);
-			});
-
-			const minTime = Math.min(...startTimes);
-			const maxTime = Math.max(...endTimes) + 1;
-
-			for (let i = minTime; i <= maxTime; i++) {
-				i > 23
-					? setTimeDuration((timeDuration) => [
-							...timeDuration,
-							("0" + (i - 24)).slice(-2) + ":00",
-					  ])
-					: setTimeDuration((timeDuration) => [
-							...timeDuration,
-							("0" + i).slice(-2) + ":00",
-					  ]);
-			}
-		};
-
-		const stageList = (responseData) => {
-			const stages = responseData.schedule.scheduleList.map((schedule) => {
-				return schedule.stage;
-			});
-			const stagesUnique = stages.filter((a, b) => stages.indexOf(a) === b);
-			setStages(stagesUnique);
-		};
-
-		const eventsByStage = (responseData) => {
-			const events = responseData.schedule.scheduleList.reduce(function (r, a) {
-				r[a.stage] = r[a.stage] || [];
-				r[a.stage].push(a);
-				return r;
-			}, {});
-			setEventList(events);
-		};
-		const orderByDay = (eventList) => {
-			[...eventList];
-		};
-		const fetchSchedule = async () => {
-			try {
-				let responseData = await sendRequest(
-					`http://localhost:5000/api/schedules/${scheduleId}`
-				);
-				setLoadedSchedule(responseData.schedule);
-				scheduleDuration(responseData);
-				stageList(responseData);
-				eventsByStage(responseData);
-				setNumDays([...Array(responseData.schedule.daysQty)]);
-				orderByDay(eventList);
-			} catch (err) {
-				console.log(err.message);
-			}
-		};
-		if (loadedSchedule) {
-			return;
-		}
-		fetchSchedule();
-	}, []);
-
-	useEffect(() => {
-		if (!loadedSchedule) return;
+	const checkTab = (loadedSchedule) => {
 		let {daysQty, startDate} = loadedSchedule;
 		if (daysQty <= 1) return;
 		const todaysDate = new Date();
@@ -118,6 +46,75 @@ const ScheduleDisplay = (props) => {
 		} else {
 			console.log("Event finished")
 		}
+	}
+
+	const scheduleDuration = (responseData) => {
+		const startTimes = responseData.scheduleList.map((schedule) => {
+			if (parseInt(schedule.startTime.split(":")[0]) < 5) {
+				return 24;
+			}
+			return parseInt(schedule.startTime.split(":")[0]);
+		});
+		const endTimes = responseData.scheduleList.map((schedule) => {
+			if (parseInt(schedule.endTime.split(":")[0], 10) < 5) {
+				return parseInt(schedule.endTime.split(":")[0]) + 24;
+			}
+			return parseInt(schedule.endTime.split(":")[0]);
+		});
+		const minTime = Math.min(...startTimes);
+		const maxTime = Math.max(...endTimes) + 1;
+		for (let i = minTime; i <= maxTime; i++) {
+			i > 23
+				? setTimeDuration((timeDuration) => [
+						...timeDuration,
+						("0" + (i - 24)).slice(-2) + ":00",
+					])
+				: setTimeDuration((timeDuration) => [
+						...timeDuration,
+						("0" + i).slice(-2) + ":00",
+					]);
+		}
+	};
+
+	const stageList = (responseData) => {
+		const stages = responseData.scheduleList.map((schedule) => {
+			return schedule.stage;
+		});
+		const stagesUnique = stages.filter((a, b) => stages.indexOf(a) === b);
+		setStages(stagesUnique);
+	};
+
+	const eventsByStage = (responseData) => {
+		const events = responseData.scheduleList.reduce(function (r, a) {
+			r[a.stage] = r[a.stage] || [];
+			r[a.stage].push(a);
+			return r;
+		}, {});
+		setEventList(events);
+	};
+
+	useEffect(() => {
+		const fetchSchedule = async () => {
+			try {
+				let responseData = await sendRequest(`http://localhost:5000/api/schedules/${scheduleId}`);
+				setLoadedSchedule(responseData.schedule);
+			} catch (err) {
+				console.log(err.message);
+			}
+		};
+		if (loadedSchedule) {
+			return;
+		}
+		fetchSchedule();
+	}, []);
+
+	useEffect(() => {
+		if (!loadedSchedule) return;
+		checkTab(loadedSchedule);
+		scheduleDuration(loadedSchedule);
+		stageList(loadedSchedule);
+		eventsByStage(loadedSchedule);
+		setNumDays([...Array(loadedSchedule.daysQty)]);
 	},[loadedSchedule]);
 
 	return (
