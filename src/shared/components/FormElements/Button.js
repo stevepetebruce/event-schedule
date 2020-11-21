98a21ecd83e1d2e01c1d031a4278641e823d65ab
stevepetebruce <stevepetebruce@hotmail.com>
2020-11-21 23:02:12 +0000

    Update link and button focus.

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -11,10 +11,10 @@ const Button = (props) => {
 					props.style
 				} button--${props.size || "default"} ${
 					props.default &&
-					"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500"
+					"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500 focus:text-white"
 				} ${
 					props.inverse &&
-					"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none focus:bg-indigo-600 button"
+					"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none button"
 				} ${
 					props.danger &&
 					"bg-red-700 hover:bg-red-600 text-white button focus:bg-red-700 focus:outline-none"
@@ -33,10 +33,10 @@ const Button = (props) => {
 					props.style
 				} button--${props.size || "default"} ${
 					props.default &&
-					"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500 button"
+					"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500 focus:text-white button"
 				} ${
 					props.inverse &&
-					"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none focus:bg-indigo-600 button"
+					"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none button"
 				} ${
 					props.danger &&
 					"bg-red-700 hover:bg-red-600 text-white button focus:bg-red-700 focus:outline-none"
@@ -51,10 +51,10 @@ const Button = (props) => {
 				props.style
 			} button--${props.size || "default"} ${
 				props.default &&
-				"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500 button"
+				"bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus:bg-indigo-500 focus:text-white button"
 			} ${
 				props.inverse &&
-				"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none focus:bg-indigo-600 button"
+				"bg-transparent hover:bg-indigo-500 text-indigo-500 border border-indigo-500 hover:border-transparent hover:text-white focus:outline-none button"
 			} ${
 				props.danger &&
 				"bg-red-700 hover:bg-red-600 text-white button focus:bg-red-700 focus:outline-none"
