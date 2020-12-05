96eb0ca5f6510aa4427d43919b4eab4469ec4ddf
stevepetebruce <stevepetebruce@hotmail.com>
2020-12-05 22:16:58 +0000

    Add account view with links to examples.

---
+++ src/user/components/Examples.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import Button from "../../shared/components/FormElements/Button";
+import {Tv, ViewList}  from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+		color: '#4B5563',
+		fontSize: "80px"
+  },
+});
+
+function Examples(props) {
+  const classes = useStyles(props);
+
+  return (
+    <div className="mt-20">
+      <h3 className='mb-4 text-center'>
+        View example
+      </h3>
+      <div className="flex flex-row justify-evenly text-center mb-4">
+        <div className="flex flex-col items-center rounded-lg border border-gray-700 w-56 pb-3 mx-1">
+          <ViewList 
+            fontSize="inherit"
+            className={classes.root}  />
+          <div className="pl-4">
+            <Button href="https://www.app.scheduled.live/timetable/5fc644e33869900017b2fb5b" inverse target="_blank" rel="noopener noreferrer">
+              VIEW SCHEDULE
+            </Button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center rounded-lg border border-gray-700 w-56 pb-3 mx-1">
+          <Tv
+            fontSize="inherit"
+            className={classes.root} />
+          <div className="pl-4">
+            <Button href="https://www.app.scheduled.live/display/5fc644e33869900017b2fb5b/1?stage=Main%20Stage" inverse target="_blank" rel="noopener noreferrer">
+              VIEW DISPLAY
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Examples
--- src/user/pages/Account.js
+++ src/user/pages/Account.js
@@ -1,7 +1,46 @@
 import React from "react";
 
+import Button from "../../shared/components/FormElements/Button";
+import Examples from "../components/Examples"
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+
 const Welcome = () => {
-	return <div className='max-w-screen-md w-full mx-auto flex mt-20'>Welcome</div>;
+	const { isLoading, error, clearError } = useHttpClient();
+
+	return (
+		<>
+			<ErrorModal error={error} onClear={clearError} />
+			{isLoading && (
+				<div className='center'>
+					<LoadingSpinner asOverlay={true} />
+				</div>
+			)}
+			{!isLoading && 
+			<div className='max-w-screen-md w-full mx-auto flex mt-24'>
+				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<div className="lg:text-center">
+						<h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Welcome</h2>
+						<h1 className='mb-4'>
+							Set up your first schedule
+						</h1>
+						<p className="my-4 max-w-2xl text-gray-500 lg:mx-auto">
+							Click below and enter your event details to view your schedule.
+						</p>
+						<Button default to='/schedule/new'>
+							Create Schedule
+						</Button>
+					</div>
+					<Examples />
+
+				</div>
+
+			</div>
+			}
+		</>
+	);
 };
 
 export default Welcome;
