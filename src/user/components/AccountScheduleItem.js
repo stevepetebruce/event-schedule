7eaf1a16a5fec26ab9034f1da9f7bcae09885e4f
stevepetebruce <stevepetebruce@hotmail.com>
2021-01-06 15:12:45 +0000

    Remove key as a prop.

--- src/user/components/AccountScheduleItem.js
+++ src/user/components/AccountScheduleItem.js
@@ -4,11 +4,11 @@ import { HashLink } from 'react-router-hash-link';
 import moment from "moment";
 import defaultImage from "../../assets/icons/no-image-icon.png"
 
-function AccountScheduleItem({schedule, userId, key}) {
+function AccountScheduleItem({schedule, userId}) {
   return (
     <li className='my-0'>
       <HashLink smooth to={`/${userId}/schedules#${schedule.id}`}>
-        <article key={key} className="p-4 flex space-x-4 hover:bg-gray-800">
+        <article className="p-4 flex space-x-4 hover:bg-gray-800">
           {schedule.logo ? <img src={schedule.logo} alt={schedule.title} className="flex-none w-12 h-12 rounded-lg object-cover bg-gray-800" /> : <img src={defaultImage} alt={schedule.title} className="flex-none w-12 h-12 rounded-lg object-cover bg-gray-800" />}
           <div className="min-w-0 relative flex-auto sm:pr-14 lg:pr-0 xl:pr-14">
             <h2 className="text-lg font-semibold mb-0.5">
